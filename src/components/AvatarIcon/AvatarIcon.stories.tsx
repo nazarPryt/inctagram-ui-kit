b88753fefc0fa9ed350876c506a1c8d20d62152a
nazarPtyt <ockap90@libero.it>
2023-12-04 17:03:55 +0100

    fix: AvatarIcon href

--- src/components/AvatarIcon/AvatarIcon.stories.tsx
+++ src/components/AvatarIcon/AvatarIcon.stories.tsx
@@ -1,19 +1,20 @@
-import {Meta, StoryObj} from '@storybook/react'
-import {AvatarIcon} from './AvatarIcon'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { AvatarIcon } from './AvatarIcon'
 
 const meta: Meta<typeof AvatarIcon> = {
-    title: 'Components/AvatarIcon',
-    component: AvatarIcon,
-    tags: ['docsPage'],
-    argTypes: {},
+  argTypes: {},
+  component: AvatarIcon,
+  tags: ['docsPage'],
+  title: 'Components/AvatarIcon',
 }
 
 export default meta
 type Story = StoryObj<typeof AvatarIcon>
 
 export const Default: Story = {
-    args: {
-        img: 'https://loremflickr.com/50/50',
-        userID: 20,
-    },
+  args: {
+    href: '/',
+    img: 'https://loremflickr.com/50/50',
+  },
 }
